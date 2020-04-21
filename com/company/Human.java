df45368728941aa03aa317db97e7ecf6a17b094a
hit02 <bagnucki02@gmail.com>
2020-04-21 17:33:14 +0200

    add sallary getter and setter

--- com/company/Human.java
+++ com/company/Human.java
@@ -3,6 +3,9 @@ package com.company;
 import com.company.devices.Car;
 import com.company.devices.Phone;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Human extends Animal {
     String firstName;
     String lastName;
@@ -21,6 +24,22 @@ public class Human extends Animal {
 
     public String toString() {
         return "Human " + firstName + " " + lastName;
+    }
+
+    public Double getSalary() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(formatter.format(date) + " Current salary: " + salary);
+        return salary;
+    }
+
+    public void setSalary(Double salary){
+        if(salary >= 0) {
+            System.out.println("Salary was sent to accountant");
+            System.out.println("ZUS and US already know that");
+            System.out.println("Please take your annex to the contract from Mrs Hanna from HR");
+            this.salary = salary;
+        }
     }
 
 }
